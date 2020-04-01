ddbba32558a151abed4d63e9e3d26745ec85d5a4
Galen Gidman <galen@galengidman.com>
2020-04-01 16:31:05 -0500

    glitch text

--- assets/js/rlo.js
+++ assets/js/rlo.js
@@ -1,17 +1,7 @@
-// (function() {
+(function() {
 
-// 	AOS.init();
+	document.querySelectorAll('a').forEach(function(link) {
+		link.dataset.text = link.innerText;
+	});
 
-// 	document.querySelectorAll('[data-lazytube]').forEach(function(lazyTube) {
-// 		lazyTube.addEventListener('click', function() {
-// 			var iframe = document.createElement('iframe');
-// 			iframe.src = 'https://www.youtube.com/embed/' + lazyTube.dataset.lazytube + '?autoplay=1&showinfo=0&rel=0';
-// 			iframe.frameBorder = '0';
-// 			iframe.allow = 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture';
-// 			iframe.allowFullscreen = true;
-
-// 			lazyTube.appendChild(iframe);
-// 		}, { once: true });
-// 	});
-
-// })();
+})();
